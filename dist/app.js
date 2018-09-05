25449e1e5bdcfacbbb9c9a1244c9d235f97a6089
fukaoi <goodakira7@gmail.com>
2018-09-06 07:08:57 +0900

    refactoring

---
+++ dist/app.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var _stellarSdk = require('stellar-sdk');
+
+var _stellarSdk2 = _interopRequireDefault(_stellarSdk);
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+_stellarSdk2.default.Network.useTestNetwork();
+var server = new _stellarSdk2.default.Server('https://horizon-testnet.stellar.org');
+var issuingKeys = _stellarSdk2.default.Keypair.fromSecret('SCZANGBA5YHTNYVVV4C3U252E2B6P6F5T3U6MM63WBSBZATAQI3EBTQ4');
+var receivingKeys = _stellarSdk2.default.Keypair.fromSecret('SDSAVCRE5JRAI7UFAVLE5IMIZRD6N6WOJUWKY4GFN34LOBEEUS4W2T2D');
+
+var jpyx = new _stellarSdk2.default.Asset('JPYX', issuingKeys.publicKey());
+
+server.loadAccount(receivingKeys.publicKey()).then(function (receiver) {
+  var transaction = new _stellarSdk2.default.TradesCallBuilder(receiver).addOperation(_stellarSdk2.default.Operation.changeTrust({
+    asset: jpyx,
+    limit: '1000'
+  })).build();
+  transaction.sign(receivingKeys);
+  return server.submitTransaction(transaction);
+}).then(function () {
+  return server.loadAccount(issuingKeys.publicKey());
+}).then(function (issuer) {
+  var transaction = new _stellarSdk2.default.TradesCallBuilder(issuer).addOperation(_stellarSdk2.default.Operation.payment({
+    destination: receivingKeys.publicKey(),
+    asset: jpyx,
+    amount: 10
+  })).build();
+  transaction.sign(issuingKeys);
+  return server.submitTransaction(transaction);
+}).catch(function (error) {
+  console.error('Stellar Error!!!', error);
+});
