b36c0c8b13114de40157e721df3434dc652dd492
fukaoi <goodakira7@gmail.com>
2018-09-08 12:29:44 +0900

    Implement

--- app/send.js
+++ app/send.js
@@ -7,5 +7,28 @@ let destinationId = 'GA2C5RFPE6GCKMY3US5PAB6UZLKIGSPIUKSLRB6Q723BM2OARMDUYEJ5';
 
 var transaction;
 
-server.loadAccount(destinationId)
+server.loadAccount(destinationId).catch(StellarSDK.NotFoundError, (error) => {
+  throw new Error('The destination account does not exist!');
+})
 
+  .then(() => {
+    return server.loadAccount(sourceKeys.publicKey())
+  })
+  .then((sourceAccount) => { 
+    transaction = new StellarSDK.TransactionBuilder(sourceAccount)
+      .addOperation(StellarSDK.Operation.payment({
+        destination: destinationId,
+        asset: StellarSDK.Asset.native(),
+        amount: '10'
+      }))
+      .addMemo(StellarSDK.Memo.text('Test Transaction')).build();
+    
+    transaction.sign(sourceKeys);
+    return server.submitTransaction(transaction);
+  }) 
+  .then((result) => {
+    console.log('Success result', result);
+  })
+  .catch((error) => {
+    console.error('Error!', error);
+  })
