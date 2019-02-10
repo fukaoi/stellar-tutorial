ab8f91e97faf575d165b546e15442a9192982685
fukaoi <goodakira7@gmail.com>
2019-02-10 17:45:11 +0900

    Impl attachment json

--- my_kyc.js
+++ my_kyc.js
@@ -1,3 +1,21 @@
 const crypto = require('crypto');
 const hash = crypto.createHash('sha256');
+const nonce = crypto.randomBytes(16)
 
+const attachment = {
+  "nonce": nonce.toString('hex'),
+  "transaction": {
+    "sender_info": {
+      "name": "Oi Fuka",
+      "address": "10B Aoyama Street",
+      "city": "Tokyo",
+      "country": "Japan"
+    }
+  },
+  "operations": [{}]
+}
+
+hash.update(JSON.stringify(attachment))
+let memoHashHex = hash.digest('hex')
+
+console.log(memoHashHex)
