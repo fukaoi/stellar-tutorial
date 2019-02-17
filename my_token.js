62236244af4f0a46b1d3919eda3271e48fdb400f
fukaoi <goodakira7@gmail.com>
2019-02-17 19:30:15 +0900

    WIP

--- my_token.js
+++ my_token.js
@@ -1,12 +1,32 @@
 const StellarSdk = require('stellar-sdk')
 const MyToken = require('./lib/token.js')
 const config = require('./config.js').config
-console.log(config.publicKey)
 
 const asset = new StellarSdk.Asset(
   'OREORE',
   config.publicKey
 );
+
+const memo = (type, value) => {
+      obj = {}
+      switch(type) {
+            case "text":
+              obj = StellarSdk.Memo.text(value)
+              break;
+            case "hash":
+              obj = StellarSdk.Memo.hash(value)
+              break;
+            case "id":
+              obj = StellarSdk.Memo.id(value)
+              break;
+            case "return":
+              obj = StellarSdk.Memo.return(value)
+              break;
+            default:
+              obj = StellarSdk.Memo.none()
+          }
+      return obj
+    }
 
 const obj = new MyToken(asset)
 obj.send(100)
