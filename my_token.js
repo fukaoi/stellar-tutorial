417d6b1e601617eca3dd6123543a6ae760690707
fukaoi <goodakira7@gmail.com>
2019-02-17 19:33:59 +0900

    Fix code format

--- my_token.js
+++ my_token.js
@@ -8,25 +8,25 @@ const asset = new StellarSdk.Asset(
 );
 
 const memo = (type, value) => {
-      obj = {}
-      switch(type) {
-            case "text":
-              obj = StellarSdk.Memo.text(value)
-              break;
-            case "hash":
-              obj = StellarSdk.Memo.hash(value)
-              break;
-            case "id":
-              obj = StellarSdk.Memo.id(value)
-              break;
-            case "return":
-              obj = StellarSdk.Memo.return(value)
-              break;
-            default:
-              obj = StellarSdk.Memo.none()
-          }
-      return obj
-    }
+  obj = {}
+  switch (type) {
+    case "text":
+      obj = StellarSdk.Memo.text(value)
+      break;
+    case "hash":
+      obj = StellarSdk.Memo.hash(value)
+      break;
+    case "id":
+      obj = StellarSdk.Memo.id(value)
+      break;
+    case "return":
+      obj = StellarSdk.Memo.return(value)
+      break;
+    default:
+      obj = StellarSdk.Memo.none()
+  }
+  return obj
+}
 
 const obj = new MyToken(asset)
 obj.send(100)
