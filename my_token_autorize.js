4c783e4064be03287c1b642e979ed5df9921307e
fukaoi <goodakira7@gmail.com>
2019-02-25 17:26:37 +0900

    modified

--- my_token_autorize.js
+++ my_token_autorize.js
@@ -2,10 +2,5 @@ const StellarSdk = require('stellar-sdk')
 const MyToken = require('./lib/token.js')
 const config = require('./config.js').config
 
-const asset = new StellarSdk.Asset(
-  'OREORE21',
-  config.publicKey
-);
 
-const obj = new MyToken(asset)
-obj.addOperation()
+
