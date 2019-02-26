cdca4b06efd4d6949718afbf9cda13bd9119c89c
fukaoi <goodakira7@gmail.com>
2019-02-26 18:38:57 +0900

    WIP

--- my_token.js
+++ my_token.js
@@ -3,7 +3,7 @@ const MyToken = require('./lib/token.js')
 const config = require('./config.js').config
 
 const asset = new StellarSdk.Asset(
-  'OREORE21',
+  'AAA',
   config.publicKey
 );
 
--- my_token_autorize.js
+++ my_token_autorize.js
@@ -3,4 +3,4 @@ const MyAuth = require('./lib/authorize.js')
 const config = require('./config.js').config
 
 const auth = new MyAuth()
-auth.run("OREORE25")
+auth.run("AAA", false)
