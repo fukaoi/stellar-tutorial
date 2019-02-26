4ea956fcfc451c48a1793f393320e92710536ed0
fukaoi <goodakira7@gmail.com>
2019-02-26 18:51:13 +0900

    Refactoring Moved to argument destionation public key and authorization falgs(true or false)

--- my_token_autorize.js
+++ my_token_autorize.js
@@ -2,5 +2,16 @@ const StellarSdk = require('stellar-sdk')
 const MyAuth = require('./lib/authorize.js')
 const config = require('./config.js').config
 
-const auth = new MyAuth()
-auth.run("AAA", false)
+if (!process.argv[2]) {
+  console.error('Input destination public key')
+  return
+}
+
+if (process.argv[3] != true || process.argv[3] != false) {
+  console.error('Input authorize true or false')
+  return
+}
+
+
+const auth = new MyAuth(process.argv[2])
+auth.run("AAA", process.argv[3])
