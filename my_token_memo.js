68bdc9cfb48e8761336bf61c4d93cb6046bc0311
fukaoi <goodakira7@gmail.com>
2019-02-27 14:46:08 +0900

    Created for sample memo code

---
+++ my_token_memo.js
@@ -0,0 +1,35 @@
+const StellarSdk = require('stellar-sdk')
+const MyToken = require('./lib/token.js')
+const config = require('./config.js').config
+
+const asset = new StellarSdk.Asset(
+  'AAA',
+  config.publicKey
+);
+
+const memo = (type, value) => {
+  let obj = {}
+  switch (type) {
+    case "text":
+      obj = StellarSdk.Memo.text(value)
+      break;
+    case "hash":
+      obj = StellarSdk.Memo.hash(value)
+      break;
+    case "id":
+      obj = StellarSdk.Memo.id(value)
+      break;
+    case "return":
+      obj = StellarSdk.Memo.return(value)
+      break;
+    default:
+      obj = StellarSdk.Memo.none()
+  }
+  return obj
+}
+const memoObj = memo("text", "I love stellar network.")
+// const memoObj = memo("hash", "b36f330c6db84bef6ca34057c9739b88115565cf873fdb1350e481140a4429ad")
+// const memoObj = memo("id", "123456789")
+// const memoObj = memo("return", "b36f330c6db84bef6ca34057c9739b88115565cf873fdb1350e481140a4429ad")
+const obj = new MyToken(asset, memoObj)
+obj.send(100)
